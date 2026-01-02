ab101a86fec10cdcdc51f03ef4937c53da5dd9d6
Md Ismail Hosen <ismailshuvo554@gmail.com>
2026-01-03 02:22:25 +0600

    splash screen structure added

--- lib/core/assets_manager.dart
+++ lib/core/assets_manager.dart
@@ -3,27 +3,26 @@ import 'package:flutter_svg/svg.dart';
 const String imagePath = "assets/images";
 const String iconPath = "assets/icons";
 const String jsonPath = "assets/json";
-const String svgPath = "assets/svg";
+const String svgPath = "assets/svgs";
 
 class ImageAssets {
   static const String splash = "$imagePath/splash_screen.png";
   static const String onboadiing1 = "$imagePath/onboarding1.png";
   static const String onboadiing2 = "$imagePath/onboarding2.png";
   static const String onboadiing3 = "$imagePath/onboarding3.png";
- 
 }
 
 class IconAssets {
   static const String appIcon = "$iconPath/app_icon.png";
-    static const String loginIcon = "$iconPath/login_top.png";
-      static const String sighupIcon = "$iconPath/signup_top.png";
- 
+  static const String anchorApp = "$iconPath/anchor_app.png";
+  static const String loginIcon = "$iconPath/login_top.png";
+  static const String sighupIcon = "$iconPath/signup_top.png";
 }
 
 class JsonAssets {}
 
 class SvgAssets {
   // ✅ General SVG Files
-  
+
   static const String logo = "$svgPath/app_logo.svg";
 }
--- lib/views/splash/splash_screen.dart
+++ lib/views/splash/splash_screen.dart
@@ -1,9 +1,36 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:wynante/core/app_colors.dart';
+import 'package:wynante/core/app_strings.dart';
+import 'package:wynante/core/assets_manager.dart';
+import 'package:wynante/core/font_manager.dart';
+import 'package:wynante/views/home/homepage_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Check authentication status after splash delay
+    Timer(const Duration(seconds: 3), () {
+      // _checkAuthAndNavigate();
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (cotext) => const HomepageScreen()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +45,43 @@ class SplashScreen extends StatelessWidget {
               AppColors.bottomLinear, // Bottom Green
             ],
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            Center(
+              child: Column(
+                spacing: 4.h,
+                children: [
+                  Container(
+                    width: 80.w,
+                    height: 80.h,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(22),
+                      color: AppColors.white,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(14.w),
+                      child: Image.asset(IconAssets.appIcon, fit: BoxFit.fill),
+                    ),
+                  ),
+
+                  Text(
+                    AppStrings.anchorUp,
+                    style: FontManager.titleText(color: AppColors.white),
+                  ),
+                  Text(
+                    AppStrings.quote,
+                    style: FontManager.heading3(
+                      color: AppColors.white,
+                      fontSize: 14.sp,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
