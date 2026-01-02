e39d3fbf86e517f667afd54a078627fb22f9fe4c
Md Ismail Hosen <ismailshuvo554@gmail.com>
2026-01-02 07:04:33 +0600

    firebase initialized

--- lib/views/splash/splash_screen.dart
+++ lib/views/splash/splash_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:wynante/core/app_colors.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              AppColors.topLinear, // Top Blue
+              AppColors.bottomLinear, // Bottom Green
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
